e6ceff392abb163fe5cbc6eb294eb2d62eaf0cf7
JC Capilla <jcrodriguezcapilla@gmail.com>
2018-03-16 20:48:04 +0100

    commit inicial. Boilerplate

---
+++ app/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {PageHeader} from 'react-bootstrap';
+import './../assets/scss/main.scss';
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <PageHeader id="heading">Gestión de proyectos de ingeniería - Grupo 16</PageHeader>
+
+    );
+  }
+
+}
+
